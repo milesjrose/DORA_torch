07dfadcd67712f07cb7146f7704c8aa9b6095d33
miles rose <milespublic@pm.me>
2026-01-07 15:06:44 +0000

    update nodeEnums init

--- nodes/enums/__init__.py
+++ nodes/enums/__init__.py
@@ -1,6 +1,6 @@
 # nodes/enums/__init__.py
 
-from .nodeEnums import SF, TF, MappingFields, Type, Set, Mode, OntStatus, B, Routines, null, tensor_type, MAPPING_SETS, TF_type, SDM, SD
+from .nodeEnums import SF, TF, MappingFields, Type, Set, Mode, OntStatus, B, Routines, null, tensor_type, MAPPING_SETS, TF_type, SDM, SD, TensorTypes
 
 __all__ = [
     "SF",
@@ -17,5 +17,6 @@ __all__ = [
     "TF_type",
     "Routines",
     "SDM",
-    "SD"
+    "SD",
+    "TensorTypes"
 ]
