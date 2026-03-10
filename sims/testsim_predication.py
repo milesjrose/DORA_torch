98020522c04a217af8089fcdb0416ca0be0c1e40
miles rose <milespublic@pm.me>
2026-03-10 19:31:32 +0000

    update sim file

--- sims/testsim_predication.py
+++ sims/testsim_predication.py
@@ -1,27 +1,24 @@
 simType='sym_file'
 symProps = [
-# ---- DRIVER (analog 0) ----
-# Objects share semantics with recipient objects; predicates are distinct.
-{'name': 'bigApple', 'RBs': [
-    {'pred_name': 'big', 'pred_sem': ['size1', 'size2', 'size3'], 'higher_order': False,
+# ---- DRIVER ----
+{'name': 'non_exist', 'RBs': [
+    {'pred_name': 'big', 'pred_sem': ['big1', 'big2', 'big3'], 'higher_order': False,
      'object_name': 'apple', 'object_sem': ['red', 'round', 'fruit'], 'P': 'non_exist'}
 ], 'set': 'driver', 'analog': 0},
 
-{'name': 'tallTree', 'RBs': [
-    {'pred_name': 'tall', 'pred_sem': ['height1', 'height2', 'height3'], 'higher_order': False,
+{'name': 'non_exist', 'RBs': [
+    {'pred_name': 'tall', 'pred_sem': ['tall1', 'tall2', 'tall3'], 'higher_order': False,
      'object_name': 'tree', 'object_sem': ['green', 'leafy', 'plant'], 'P': 'non_exist'}
 ], 'set': 'driver', 'analog': 0},
 
-# ---- RECIPIENT (analog 1) ----
-# Objects share semantics with driver objects (red/round, green/leafy);
-# predicates have no semantic overlap with driver predicates.
-{'name': 'niceBall', 'RBs': [
-    {'pred_name': 'nice', 'pred_sem': ['nice1', 'nice2', 'nice3'], 'higher_order': False,
+# ---- RECIPIENT ----
+{'name': 'non_exist', 'RBs': [
+    {'pred_name': 'big', 'pred_sem': ['big1', 'big2', 'big3'], 'higher_order': False,
      'object_name': 'ball', 'object_sem': ['red', 'round', 'toy'], 'P': 'non_exist'}
 ], 'set': 'recipient', 'analog': 1},
 
-{'name': 'goodBush', 'RBs': [
-    {'pred_name': 'good', 'pred_sem': ['good1', 'good2', 'good3'], 'higher_order': False,
+{'name': 'non_exist', 'RBs': [
+    {'pred_name': 'tall', 'pred_sem': ['tall1', 'tall2', 'tall3'], 'higher_order': False,
      'object_name': 'bush', 'object_sem': ['green', 'leafy', 'shrub'], 'P': 'non_exist'}
 ], 'set': 'recipient', 'analog': 1},
 ]
